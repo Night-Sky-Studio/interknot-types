c782b1d93f0260de251322d3cbf657cad9cf52d7
LilyStilson <lunamusicoff@gmail.com>
2025-02-27 18:45:09 +0100

    fix: added PropertyType export; bump(version): 0.1.2-beta

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import type { DriveDisc } from "./types/DriveDisc"
 import type { Equipment } from "./types/Equipment"
 import type { Medal } from "./types/Medal"
 import type { Profile } from "./types/Profile"
-import type { Property } from "./types/Property"
+import type { Property, PropertyType } from "./types/Property"
 import type { Skin } from "./types/Skin"
 import type { Title } from "./types/Title"
 import type { Weapon } from "./types/Weapon"
@@ -20,6 +20,7 @@ export type {
     Medal,
     Profile,
     Talents,
+    PropertyType,
     Property,
     Skin,
     Title,
