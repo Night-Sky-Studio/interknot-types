20f0723ac027b906dcd48b0611865b00810dcbe8
LilyStilson <lunamusicoff@gmail.com>
2025-03-05 19:56:46 +0100

    impl: added leaderboard types

---
+++ types/Leaderboard.ts
@@ -0,0 +1,38 @@
+import type { BaseAvatar } from "./Avatar"
+import type { BaseWeapon } from "./Weapon"
+
+export interface RawWeaponData { 
+    weapons: WeaponEntry[] 
+}
+
+export interface RawLeaderboardProfile {
+    agents: Record<number, RawWeaponData>
+    cached: boolean
+    response_time: number
+    uid: number
+}
+
+export interface WeaponData extends BaseWeapon {
+    Id: number
+    Damage: number
+    Players: number
+    Rank: number
+}
+
+export interface WeaponEntry {
+    Id: number
+    Damage: number
+    Players: number
+    Rank: number
+}
+
+export interface LeaderboardAgent {
+    Agent: BaseAvatar
+    Weapons: WeaponData[]
+}
+
+export interface LeaderboardProfile {
+    Uid: number
+    Cached: boolean
+    Agents: LeaderboardAgent[]
+}
