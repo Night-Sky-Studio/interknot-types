c956b2c2db52ac9a9784262457cb72a83bfc89bf
LilyStilson <lunamusicoff@gmail.com>
2025-03-20 21:27:36 +0100

    fix: changed leaderboard profile API; bump: v0.2.1-beta

--- types/Leaderboard.ts
+++ types/Leaderboard.ts
@@ -1,40 +1,20 @@
 import type { BaseAvatar } from "./Avatar"
 import type { BaseWeapon } from "./Weapon"
 
-export interface RawWeaponData { 
-    weapons: WeaponEntry[] 
-}
-
-export interface RawLeaderboardProfile {
-    agents: Record<number, RawWeaponData>
-    cached: boolean
-    response_time: number
-    uid: number
-}
-
-export interface WeaponData extends BaseWeapon {
-    Id: number
-    BuildName: string
-    Damage: number
-    Players: number
-    Rank: number
-}
-
-export interface WeaponEntry {
-    Id: number
-    Name: string
-    Damage: number
-    Players: number
-    Rank: number
-}
-
 export interface LeaderboardAgent {
+    LeaderboardId: number
+    LeaderboardName: string
     Agent: BaseAvatar
-    Weapons: WeaponData[]
+    Weapon: BaseWeapon
+    Team: BaseAvatar[]
+    Rank: number
+    Total: number
+    Rotation: string[]
+    RotationValue: number[]
+    TotalValue: number
 }
 
 export interface LeaderboardProfile {
     Uid: number
-    Cached: boolean
     Agents: LeaderboardAgent[]
 }
