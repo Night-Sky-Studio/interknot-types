8dac12be6b11e004c3a8588e0c4f4f00b7a08107
Konstantin Romanets <konstantin.romanets@jetbrains.com>
2025-12-26 15:31:43 +0100

    feat: added AuthProvider

--- types/Account.ts
+++ types/Account.ts
@@ -1,5 +1,10 @@
 import { type ProfileInfo } from "./Profile"
 
+export enum AuthProvider {
+    Discord = "discord",
+    Patreon = "patreon"
+}
+
 export interface Account {
     Id: number
     Username: string
@@ -8,6 +13,6 @@ export interface Account {
     PatreonStatus?: boolean
     EnkaPatreonStatus?: boolean
     ClaimedProfiles: ProfileInfo[]
-    Provider: "discord" | "patreon"
+    Provider: AuthProvider
     LinkedAccount?: Account
 }
